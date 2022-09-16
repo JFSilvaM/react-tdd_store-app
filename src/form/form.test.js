86e987e70590d8198f22b1757396c9bfc3390f4e
joaofsma <joaofsma@ext.inditex.com>
2022-09-16 12:14:53 +0200

    first test

---
+++ src/form/form.test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {screen, render} from '@testing-library/react'
+import {Form} from './form'
+
+describe('when the form is mounted', () => {
+  test('there must be a create product from page', () => {
+    render(<Form />)
+
+    expect(
+      screen.getByRole('heading', {name: /create product/i}),
+    ).toBeInTheDocument()
+  })
+})
